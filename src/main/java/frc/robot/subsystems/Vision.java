38e12b0a4841a33e98ff3c1c1dc2d4501e64f13a
DivineJr <divineholinessjr@gmail.com>
2026-03-08 16:38:41 -0400

    Was too lazy to switch to mine mb, got feedforward on bottom shooter and updated CAN ID's in RobotMap

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import org.photonvision.PhotonPoseEstimator;
+// import org.photonvision.PhotonPoseEstimator;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap.Controller;
 
 public class Vision extends SubsystemBase {
-    PhotonPoseEstimator test = new PhotonPoseEstimator(AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded), null);
+    // PhotonPoseEstimator test = new PhotonPoseEstimator(AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded), null);
     // Photon
 
 
